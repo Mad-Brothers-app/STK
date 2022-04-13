f471598cd8c80a9b3c4a89efcdc7f71a4980a28c
Gawsan <gawsan98@gmail.com>
2022-04-13 10:34:41 +0530

    register,create new account activity fixes

--- app/src/main/java/com/example/busseatreserve/register_driver.java
+++ app/src/main/java/com/example/busseatreserve/register_driver.java
@@ -2,13 +2,27 @@ package com.example.busseatreserve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class register_driver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_register_driver);
+    }
+
+    //passing the activity to login
+    public void loginUser(View view) {
+        Intent i = new Intent(this, driverLogin.class);
+        startActivity(i);
     }
 }
